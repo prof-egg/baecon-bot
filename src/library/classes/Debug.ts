f00578419bbeda2c99d4d36042774aa7016f7988
Cryptic42069 <69015627+Cryptic42069@users.noreply.github.com>
2023-11-27 15:02:21 -0500

    Added basic log function

--- src/library/classes/Debug.ts
+++ src/library/classes/Debug.ts
@@ -10,4 +10,9 @@ export class Debug {
         if (logger) return Util.colorLog("yellow", `[${Util.consoleTimeStamp()}] [${logger}/WARNING]: ${message}`)
         return Util.colorLog("yellow", `[${Util.consoleTimeStamp()}] [WARNING]: ${message}`)
     }
+
+    static log(message: string, logger?: string): void {
+        if (logger) return console.log(`[${Util.consoleTimeStamp()}] [${logger}/ERROR]: ${message}`)
+        return console.log(`[${Util.consoleTimeStamp()}] [ERROR]: ${message}`)
+    }
 }
